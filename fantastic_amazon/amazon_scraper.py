9c48929c3cbbb57e2feed29627c7866fe46ce7da
mschrader15 <mcschrader@crimson.ua.edu>
2021-06-06 20:41:01 -0500

    code can pull search results

--- fantastic_amazon/amazon_scraper.py
+++ fantastic_amazon/amazon_scraper.py
@@ -36,7 +36,6 @@ def proxy_generator():
     return proxies
 
 
-
 def change_proxy(proxy_info, ua, headless=False):
     "Define Firefox Profile with you ProxyHost and ProxyPort"
     profile = webdriver.FirefoxProfile()
@@ -63,35 +62,45 @@ class AmazonScrapper:
         self.browser = None
         self.headless = False
 
+    def _get_search_results(self, url, search_terms):
+        self.browser = change_proxy(random.choice(
+            self.proxies), ua.random, self.headless)
+        self.browser.get(url)
+        search_bar = self.browser.find_element_by_id('twotabsearchtextbox')
+        search_bar.send_keys(search_terms)
+        suggestions = self.browser.find_element_by_id('suggestions')
+        self.browser.find_element_by_id('nav-search-submit-button').click()
+        return self.browser.page_source, suggestions.text.split('\n')
+
     def requester(self, url, page_num, try_count=0, ):
-        self.browser = change_proxy(random.choice(self.proxies), ua.random, self.headless) if not self.browser else self.browser
+        self.browser = change_proxy(random.choice(
+            self.proxies), ua.random, self.headless) if not self.browser else self.browser
         while True:
             if page_num <= 1:
-                self.browser.get(url)  
+                self.browser.get(url)
             else:
                 refresh_count = 0
-                while True: 
+                while True:
                     try:
-                        self.browser.find_element_by_css_selector('.a-last > a:nth-child(1)').click()
+                        self.browser.find_element_by_css_selector(
+                            '.a-last > a:nth-child(1)').click()
                         break
                     except selenium.common.exceptions.NoSuchElementException:
                         self.browser.refresh()
                         refresh_count += 1
                         if refresh_count > 5:
                             self.browser.get(url)
-    
             response = self.browser.page_source
-    
-            if "api-services-support@amazon.com" in response:                               
+            if "api-services-support@amazon.com" in response:
                 if try_count > 5:
-                    raise Exception("CAPTCHA is not bypassed")                              
+                    raise Exception("CAPTCHA is not bypassed")
                 self.close_browser()
                 self.requester(url, page_num=page_num, try_count=try_count+1)
             else:
                 break
-    
+
         time.sleep(random.uniform(3, 10))
-    
+
         return response
 
     def generate_url(self, amazon_site, product_asin, page_number):
@@ -99,13 +108,14 @@ class AmazonScrapper:
 
     def generate_product_url(self, amazon_site, product_asin,):
         return "".join(["https://www.", amazon_site, "/dp/", product_asin])
-    
+
     def scrape(self, amazon_site, url=None, asin=None, headless=False):
         count = 0
         self.headless = headless
         while True:
             try:
-                reviews = {"date_info": [], "name": [], "title": [], "content": [], "rating": []}
+                reviews = {"date_info": [], "name": [],
+                           "title": [], "content": [], "rating": []}
                 asin = self.find_asin(url) if url else asin
                 url_1 = self.generate_url(amazon_site, asin, 1)
                 print(f"scraping: {url_1}")
@@ -113,9 +123,11 @@ class AmazonScrapper:
                 total_pages = self._get_total_pages(page_1)
                 for page in range(total_pages):
                     url = self.generate_url(amazon_site, asin, page + 1)
-                    response = page_1 if page < 1 else self.requester(url, page_num=page)
-                    try:    
-                        self._page_scraper(response=response, reviews_dict=reviews)
+                    response = page_1 if page < 1 else self.requester(
+                        url, page_num=page)
+                    try:
+                        self._page_scraper(
+                            response=response, reviews_dict=reviews)
                     except Exception as e:
                         print(e, url)
                         pass
@@ -127,6 +139,23 @@ class AmazonScrapper:
                 if count > 5:
                     raise Exception("Pulling Reviews Failed")
 
+    def scrape_search(self, amazon_site, search_terms, headless=False):
+        count = 0
+        self.headless = headless
+        while True:
+            try:
+                response, search_suggestions = self._get_search_results(
+                    url="".join(["https://www.", amazon_site]), search_terms=search_terms)
+                results = self._search_page_scraper(response)
+                results['suggestions'] = search_suggestions
+                self.close_browser()
+                return results
+            except Exception as e:
+                print(f"Errored out on {url_1}", e)
+                count += 1
+                if count > 5:
+                    self.close_browser()
+                    raise Exception("Pulling Reviews Failed")
 
     def scrape_product_page(self, amazon_site, url=None, asin=None, headless=False):
         self.headless = headless
@@ -136,13 +165,14 @@ class AmazonScrapper:
                 asin = self.find_asin(url) if url else asin
                 url_1 = self.generate_product_url(amazon_site, asin)
                 page_1 = self.requester(url_1, page_num=1)
+                self.close_browser()
                 return self._product_page_scraper(page_1, )
             except Exception as e:
-                print(e)
+                print(f"Errored out on {url_1}", e)
                 count += 1
                 if count > 5:
+                    self.close_browser()
                     raise Exception("Pulling Reviews Failed")
-        
 
     def close_browser(self, ):
         try:
@@ -151,26 +181,53 @@ class AmazonScrapper:
             pass
         self.browser = None
 
+    def _search_page_scraper(self, response: requests.Response):
+        search_page = BeautifulSoup(response, 'html.parser')
+        product_cards = search_page.findAll('div', {'class': 's-asin'})
+        results = {}
+        for product in product_cards:
+            index = product.attrs['data-index']
+            results[index] = {
+                'asin': product.attrs['data-asin'],
+                'ad': 'AdHolder' in product.attrs['class'],
+                'amazon_choice': "Amazon's Choice" in product.text
+            }
+        return results
+
     def _product_page_scraper(self, response: requests.Response):
         product_page = BeautifulSoup(response, 'html.parser')
         title = product_page.select('#productTitle')[0].text.strip()
-        price = product_page.select('#priceblock_ourprice')[0].text.strip()
-        bullets = [span.txt for span in product_page.select('#feature-bullets')[0].findAll("span", {"class": "a-list-item"}) if not any('hidden' in _cls for _cls in span.previous.attrs['class'])]
-        aplus_content = product_page.findAll("div", {"id": "aplus3p_feature_div"})[0].text.strip()
-        rating = product_page.findAll("span", {"data-hook": "rating-out-of-text"})[0].text.split(' out')[0]
+        price = None
+        for _id in ['#priceblock_ourprice', '#priceblock_dealprice', '#priceblock_saleprice']:
+            try:
+                price = product_page.select(_id)[0].text.strip()
+                break
+            except IndexError:
+                pass
+        bullets = [span.text for span in product_page.select('#feature-bullets')[0].findAll(
+            "span", {"class": "a-list-item"}) if not 'class' in span.previous.attrs]
+        aplus_content = product_page.findAll(
+            "div", {"id": "aplus"})[0].text.strip()
+        rating = product_page.findAll(
+            "span", {"data-hook": "rating-out-of-text"})[0].text.split(' out')[0]
         return {'title': title, 'price': price, 'bullets': bullets, 'aplus_content': aplus_content, 'rating': rating}
-        
+
     def _page_scraper(self, response: requests.Response, reviews_dict: dict):
         reviews = BeautifulSoup(response, 'html.parser').findAll(
             "div", {"class": "a-section review aok-relative"})
         reviews = BeautifulSoup(
             '<br/>'.join([str(tag) for tag in reviews]), 'html.parser')
 
-        titles = _helper(reviews, ["a", "span"], "class", "review-title", sort_func=lambda x: x.find('span').text)
-        names = _helper(reviews, "span", "class", "a-profile-name", sort_func=lambda x: x.text)
-        ratings = _helper(reviews, ["i", ], "data-hook", ["review-star-rating", "cmps-review-star-rating"], sort_func=lambda x: float(x.text.split(' out')[0]))
-        date_list = _helper(reviews, "span", "data-hook", "review-date", sort_func=lambda x: x.text.split('on ')[-1])
-        contents = _helper(reviews, "span", "data-hook", "review-body", sort_func=lambda x: x.text.strip())
+        titles = _helper(reviews, [
+                         "a", "span"], "class", "review-title", sort_func=lambda x: x.find('span').text)
+        names = _helper(reviews, "span", "class",
+                        "a-profile-name", sort_func=lambda x: x.text)
+        ratings = _helper(reviews, ["i", ], "data-hook", ["review-star-rating",
+                          "cmps-review-star-rating"], sort_func=lambda x: float(x.text.split(' out')[0]))
+        date_list = _helper(reviews, "span", "data-hook", "review-date",
+                            sort_func=lambda x: x.text.split('on ')[-1])
+        contents = _helper(reviews, "span", "data-hook",
+                           "review-body", sort_func=lambda x: x.text.strip())
 
         reviews_dict['date_info'].extend(date_list)
         reviews_dict['name'].extend(names)
@@ -197,5 +254,4 @@ class AmazonScrapper:
 
 if __name__ == "__main__":
 
-    AmazonScrapper().scrape_product_page("amazon.com", url="https://www.amazon.com/dp/B00ECDM78E/")
-    
+    AmazonScrapper().scrape_search("amazon.com", search_terms="Triple Display Dock")
